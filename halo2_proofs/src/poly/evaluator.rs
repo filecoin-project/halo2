d90523dfa288b2619d83147916f7edc7c2a8360e
Volker Mische <volker.mische@gmail.com>
2022-12-19 17:42:52 +0100

    fix: use GPU code again

--- halo2_proofs/src/poly/evaluator.rs
+++ halo2_proofs/src/poly/evaluator.rs
@@ -448,14 +448,14 @@ impl<E, F: Field, B: Basis> Evaluator<E, F, B> {
         //    self.evaluate_cpu(ast, domain)
         //}
 
-        //#[cfg(not(any(feature = "cuda", feature = "opencl")))]
-        //{
-        self.evaluate_cpu(ast, domain)
-        //}
-        //#[cfg(any(feature = "cuda", feature = "opencl"))]
-        //{
-        //    self.evaluate_gpu(ast, domain)
-        //}
+        #[cfg(not(any(feature = "cuda", feature = "opencl")))]
+        {
+            self.evaluate_cpu(ast, domain)
+        }
+        #[cfg(any(feature = "cuda", feature = "opencl"))]
+        {
+            self.evaluate_gpu(ast, domain)
+        }
     }
 
     /// Evaluates the given polynomial operation against this context.
